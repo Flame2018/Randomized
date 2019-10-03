486a1f58da60e20e511ac3add5db1c79a9267478
Flame2018 <54338887+Flame2018@users.noreply.github.com>
2019-10-03 12:50:32 -0500

    Add files via upload

---
+++ 2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FOMapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string[] random { get; set; }
+        public void OnGet()
+        {
+            random = new string[] { "Soil", "water bottles", "markets", "blazers", "Vine" };
+        }
+    }
+}
